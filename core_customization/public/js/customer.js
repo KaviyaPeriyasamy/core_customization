f9158e558bfa57ee588fd286035a77133a6e3464
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-06-10 13:24:44 +0530

    fix:hook

--- core_customization/public/js/customer.js
+++ core_customization/public/js/customer.js
@@ -10,12 +10,12 @@ frappe.ui.form.on('Customer', {
                 'customer': frm.doc.name
             },
             callback: function(data){
-                if (r.message) {
+                if (data.message) {
                     if(frm.doc.cf_no_of_newly_created_entries) {
                         let section = frm.dashboard.add_section(
                             frappe.render_template('core_customization/custom_scripts/html/customer-dashboard.html', {
-                                created_events: r.message[0],
-                                assigned_events: r.message[1]
+                                created_events: data.message[0],
+                                assigned_events: data.message[1]
                             })
                         );
                         frm.dashboard.show();
