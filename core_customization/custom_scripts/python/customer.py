f27b5efb24246da3bf9af2ee4a5dee315f26d2a0
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-06-10 13:30:02 +0530

    fix:hook

--- core_customization/custom_scripts/python/customer.py
+++ core_customization/custom_scripts/python/customer.py
@@ -2,6 +2,7 @@ import frappe
 
 @frappe.whitelist()
 def get_events(customer):
+	frappe.log_error(customer)
 	created_event_list = frappe.db.get_all('Event Participants', {'reference_doctype':'Customer', 'reference_docname':customer},['parent'])
 	assigned_event_list = frappe.db.get_all('Event', {'created_by': customer})
 	return [created_event_list, assigned_event_list]
